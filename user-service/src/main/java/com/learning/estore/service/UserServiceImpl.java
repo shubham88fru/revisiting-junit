79c6dd14a5c551dc96b9a61b51b82e57185224b8
shubham88fru <shubham88fru@yahoo.com>
2025-05-29 14:43:24 -0700

    refactor

--- user-service/src/main/java/com/learning/estore/service/UserServiceImpl.java
+++ user-service/src/main/java/com/learning/estore/service/UserServiceImpl.java
@@ -10,6 +10,14 @@ public class UserServiceImpl implements UserService {
     public User createUser(String firstName, String lastName,
                            String email, String password, String repeatPassword) {
 
+        if (firstName == null || lastName == null || email == null || password == null || repeatPassword == null) {
+            throw new IllegalArgumentException("All required parameters are null");
+        }
+
+        if (firstName.isEmpty() || lastName.isEmpty() || email.isEmpty() || password.isEmpty() || repeatPassword.length() == 0) {
+            throw new IllegalArgumentException("All required parameters are empty");
+        }
+
         return new User(
                 firstName,
                 lastName,
--- user-service/src/test/java/com/learning/estore/service/UserServiceTest.java
+++ user-service/src/test/java/com/learning/estore/service/UserServiceTest.java
@@ -2,21 +2,32 @@ package com.learning.estore.service;
 
 import com.learning.estore.model.User;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class UserServiceTest {
 
+    UserService userService;
+    String firstName;
+    String lastName;
+    String email;
+    String password;
+    String repeatPassword;
+
+    @BeforeEach
+    void init() {
+        userService = new UserServiceImpl();
+        firstName = "shubham";
+        lastName = "singh";
+        email = "test@test.com";
+        password = "password";
+        repeatPassword = "password";
+    }
+
     @DisplayName("User object created")
     @Test
     public void testCreateUser_whenUserDetailsProvided_returnsUserObject() {
-        //arrange
-        UserService userService = new UserServiceImpl();
-        String firstName = "shubham";
-        String lastName = "singh";
-        String email = "test@test.com";
-        String password = "password";
-        String repeatPassword = "password";
 
         //act
         User user = userService.createUser(firstName, lastName,
@@ -39,5 +50,19 @@ public class UserServiceTest {
 
         Assertions.assertNotNull(user.getId(), "User id is missing");
 
+    }
+
+    @DisplayName("Empty first name causes correct exception")
+    @Test
+    public void testCreateUser_whenFirstNameIsEmpty_throwsIllegalArgumentException() {
+        // Arrange
+        firstName = "";
+
+        // Act and assert
+        Assertions.assertThrows(IllegalArgumentException.class, () -> userService.createUser(
+                firstName, lastName, email, password, repeatPassword
+        ), "Empty first name should have caused an Illegal argument exception.");
+
+        // Assertions
     }
 }
